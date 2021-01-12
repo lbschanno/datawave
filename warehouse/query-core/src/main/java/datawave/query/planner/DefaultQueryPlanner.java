37fe0a625d68b4cb90017b1a702b08d78590f2e9
Laura Schanno <lbschanno@gmail.com>
2021-01-11 23:13:57 -0500

    Count QueryPropertyMarker types

--- warehouse/query-core/src/main/java/datawave/query/planner/DefaultQueryPlanner.java
+++ warehouse/query-core/src/main/java/datawave/query/planner/DefaultQueryPlanner.java
@@ -1194,7 +1194,7 @@ public class DefaultQueryPlanner extends QueryPlanner implements Cloneable {
                     log.debug("No regex to expand");
                 }
                 
-                if (nodeCount.hasPossibleBoundedRange()) {
+                if (nodeCount.hasBoundedRange()) {
                     innerStopwatch = timers.newStartedStopwatch("DefaultQueryPlanner - Expand ranges");
                     queryTree = RangeConjunctionRebuildingVisitor.expandRanges(config, scannerFactory, metadataHelper, queryTree, config.isExpandFields(),
                                     config.isExpandValues());
@@ -1261,7 +1261,7 @@ public class DefaultQueryPlanner extends QueryPlanner implements Cloneable {
                         }
                     }
                     
-                    if (nodeCount.hasPossibleBoundedRange()) {
+                    if (nodeCount.hasBoundedRange()) {
                         queryTree = RangeConjunctionRebuildingVisitor.expandRanges(config, scannerFactory, metadataHelper, queryTree, config.isExpandFields(),
                                         config.isExpandValues());
                         if (log.isDebugEnabled()) {
